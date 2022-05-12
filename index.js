2833824c2abd3e2d8b4139ee4283911f46802907
Santi Arroyo <santi.arroyo551@gmail.com>
2022-05-12 11:42:04 -0400

    commit tree 2cd780a7d756001627f5741f7e0b75213c8437d3

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
 function addingEventListener() {
+    input.addEventListener('click', clickAlert);
 }
